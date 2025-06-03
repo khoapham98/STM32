08321cf67af8bd35b640cecd8297186c55f8876f
anhkh04 <phamanhkhoa9803@gmail.com>
2025-06-03 20:34:05 +0700

    Use HAL library to configure input and output pins

--- project1/Core/Src/main.c
+++ project1/Core/Src/main.c
@@ -1,116 +1,236 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2025 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
 #include "main.h"
-#include <string.h>
-#define GPIOD_BASE_ADDR 0x40020C00
-#define GPIOA_BASE_ADDR 0x40020000
-#define GPIOB_BASE_ADDR 0x40020400
-#define EXTI_BASE_ADDR 0x40013C00
 
-typedef enum{
-	GREEN,
-	ORANGE,
-	RED,
-	BLUE
-} LED_color;
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
 
-typedef enum{
-	OFF,
-	ON
-} LED_state;
+/* USER CODE END Includes */
 
-void LEDsInit()
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+
+/* USER CODE BEGIN PV */
+
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+/* USER CODE BEGIN PFP */
+
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+char isPressed()
 {
-	__HAL_RCC_GPIOD_CLK_ENABLE();
-	uint32_t* GPIOD_MODER = GPIOD_BASE_ADDR + 0x00;
-	// set PD12, PD13, PD14, PD15 OUTPUT
-	*GPIOD_MODER &= ~(0b11111111 << 24);
-	*GPIOD_MODER |= (0b01010101 << 24);
-}
-void ButtonInit()
-{
-	__HAL_RCC_GPIOB_CLK_ENABLE();
-	uint32_t* GPIOB_MODER = GPIOB_BASE_ADDR + 0x00;
-	// set PB4 as INPUT
-	*GPIOB_MODER &= ~(0b11 << 8);
-}
-int isPressed()
-{
-	uint32_t* GPIOB_IDR = GPIOB_BASE_ADDR + 0x10;
-
-	if (((*GPIOB_IDR >> 4) & 0b1) == 0b1)
+	if (((GPIOC->IDR >> 8) & 0b1) == 0b1)
 	{
 		return 1;
 	}
-
 	return 0;
 }
-void ctrlLEDs(LED_color color, LED_state state)
-{
-	uint32_t* GPIOD_ODR = GPIOD_BASE_ADDR + 0x14;
-	if (state == ON)
-	{
-		*GPIOD_ODR |= (1 << (color + 12));
-	}
-	else
-	{
-		*GPIOD_ODR &= ~(1 << (color + 12));
-	}
-}
 
-void EXTI_Init()
-{
-	uint32_t* EXTI4_FTSR = EXTI_BASE_ADDR + 0x0C;
-	uint32_t* EXTI4_RTSR = EXTI_BASE_ADDR + 0x08;
-	*EXTI4_FTSR |= (0b1 << 4);
-	*EXTI4_RTSR |= (0b1 << 4);
-
-	uint32_t* EXTI4_IMR = EXTI_BASE_ADDR + 0x00;
-	*EXTI4_IMR |= (0b1 << 4);
-
-	uint32_t* NVIC_ISER0 = 0xE000E100;
-	*NVIC_ISER0 |= (0b1 << 10);
-
-	uint32_t* VTOR = 0xE000ED08;
-}
-
-void EXTI4_IRQHandler()
+void ctrl_LED()
 {
 	if (isPressed())
 	{
-		ctrlLEDs(BLUE, ON);
+		GPIOD->ODR |= (0b1 << 15);
 	}
 	else
 	{
-		ctrlLEDs(BLUE, OFF);
+		GPIOD->ODR &= ~(0b1 << 15);
 	}
-
-	uint32_t* EXTI4_PR = EXTI_BASE_ADDR + 0x14;
-	*EXTI4_PR |= (0b1 << 4);
 }
-
-int main()
+int main(void)
 {
-	HAL_Init();
-	LEDsInit();
-	ButtonInit();
-	EXTI_Init();
 
-	void (*fptr)() = EXTI4_IRQHandler;
+  /* USER CODE BEGIN 1 */
 
-	while (1)
-	{
-		ctrlLEDs(RED, ON);
-		HAL_Delay(500);
-		ctrlLEDs(RED, OFF);
-		HAL_Delay(500);
-//		if (isPressed())
-//		{
-//			ctrlLEDs(RED, ON);
-//		}
-//		else
-//		{
-//			ctrlLEDs(RED, OFF);
-//		}
-	}
+  /* USER CODE END 1 */
 
-	return 0;
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  /* USER CODE BEGIN 2 */
+
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  while (1)
+  {
+    /* USER CODE END WHILE */
+	  ctrl_LED();
+    /* USER CODE BEGIN 3 */
+  }
+  /* USER CODE END 3 */
 }
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Configure the main internal regulator output voltage
+  */
+  __HAL_RCC_PWR_CLK_ENABLE();
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/**
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_GPIO_Init(void)
+{
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+  /* USER CODE BEGIN MX_GPIO_Init_1 */
+
+  /* USER CODE END MX_GPIO_Init_1 */
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOD_CLK_ENABLE();
+  __HAL_RCC_GPIOC_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOD, GPIO_PIN_15, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin : PD15 */
+  GPIO_InitStruct.Pin = GPIO_PIN_15;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOD, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : PC8 */
+  GPIO_InitStruct.Pin = GPIO_PIN_8;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+
+  /* USER CODE BEGIN MX_GPIO_Init_2 */
+
+  /* USER CODE END MX_GPIO_Init_2 */
+}
+
+/* USER CODE BEGIN 4 */
+
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
