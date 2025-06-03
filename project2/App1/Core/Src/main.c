effdd4a3cb4ea5408ad8f9c01fdec2d194c2dc6a
anhkh04 <phamanhkhoa9803@gmail.com>
2025-06-03 16:19:50 +0700

    copy the vector table of App1 to RAM, define a custom_Handler()

--- project2/App1/Core/Src/main.c
+++ project2/App1/Core/Src/main.c
@@ -8,6 +8,75 @@ typedef enum{
 	OFF, ON
 } LED_state;
 
+void LED_Init();
+void LED_ctrl(LED_state state);
+void myDelay();
+void custom_Handler();
+void EXTI_Init();
+
+int main()
+{
+	HAL_Init();
+	LED_Init();
+	EXTI_Init();
+
+	while (1)
+	{
+		LED_ctrl(ON);
+		myDelay();
+		LED_ctrl(OFF);
+		myDelay();
+	}
+
+	return 0;
+}
+
+void custom_Handler()
+{
+	// Reset the PR Register to turn off the interrupt flag
+	uint32_t* EXTI_PR = (uint32_t*) (EXTI_BASE_ADDR + 0x14);
+	*EXTI_PR |= (0b1 << 0);
+
+	// Turn off Red LED before jump
+	LED_ctrl(OFF);
+
+	// Jump to Reset Handler function of App2
+	uint32_t* ptr = (uint32_t*) 0x08008004;
+	void (*pf)() = (void (*)()) *ptr;
+
+	pf();
+}
+
+void EXTI_Init()
+{
+	__HAL_RCC_GPIOA_CLK_ENABLE();
+	// Declare GPIOA's MODER Register
+	uint32_t* GPIOA_MODER = (uint32_t*) (GPIOA_BASE_ADDR + 0x00);
+
+	// Set pin PA0 as INPUT
+	*GPIOA_MODER &= ~(0b11 << 0);
+
+	// Declare EXTI line 0's FTSR Register and set to HIGH
+	uint32_t* EXTI_FTSR = (uint32_t*) (EXTI_BASE_ADDR + 0x0C);
+	*EXTI_FTSR |= (0b1 << 0);
+
+	// Declare EXTI line 0's IMR Register and set to HIGH
+	uint32_t* EXTI_IMR = (uint32_t*) (EXTI_BASE_ADDR + 0x00);
+	*EXTI_IMR |= (0b1 << 0);
+
+	// Set the NVIC ISER0 at position 6 to HIGH
+	uint32_t* NVIC_ISER0 = (uint32_t*) 0xE000E100;
+	*NVIC_ISER0 |= (0b1 << 6);
+
+	memcpy(0x20000000, 0x08004000, 0x198);
+
+	uint32_t* VTOR = (uint32_t*) 0xE000ED08;
+	*VTOR = 0x20000000;
+
+	uint32_t* fp = (uint32_t*) 0x20000058;
+	*fp = custom_Handler;
+}
+
 void LED_Init()
 {
 	__HAL_RCC_GPIOD_CLK_ENABLE();
@@ -34,48 +103,6 @@ void LED_ctrl(LED_state state)
 	}
 }
 
-void EXTI_Init()
-{
-	__HAL_RCC_GPIOA_CLK_ENABLE();
-	// Declare GPIOA's MODER Register
-	uint32_t* GPIOA_MODER = (uint32_t*) (GPIOA_BASE_ADDR + 0x00);
-
-	// Set pin PA0 as INPUT
-	*GPIOA_MODER &= ~(0b11 << 0);
-
-	// Declare EXTI line 0's FTSR Register and set to HIGH
-	uint32_t* EXTI_FTSR = (uint32_t*) (EXTI_BASE_ADDR + 0x0C);
-	*EXTI_FTSR |= (0b1 << 0);
-
-	// Declare EXTI line 0's IMR Register and set to HIGH
-	uint32_t* EXTI_IMR = (uint32_t*) (EXTI_BASE_ADDR + 0x00);
-	*EXTI_IMR |= (0b1 << 0);
-
-	// Set the NVIC ISER0 at position 6 to HIGH
-	uint32_t* NVIC_ISER0 = (uint32_t*) 0xE000E100;
-	*NVIC_ISER0 |= (0b1 << 6);
-}
-
-void EXTI0_IRQHandler()
-{
-	// Reset the PR Register to turn off the interrupt flag
-	uint32_t* EXTI_PR = (uint32_t*) (EXTI_BASE_ADDR + 0x14);
-	*EXTI_PR |= (0b1 << 0);
-
-	// Turn off Red LED before jump
-	LED_ctrl(OFF);
-
-	// Let ARM know that the vector table has been moved to 0x0800 8000 (beginning off App2 firmware)
-	uint32_t* VTOR = (uint32_t*) 0xE000ED08;
-	*VTOR = 0x08008000;
-
-	// Jump to Reset Handler function of App2
-	uint32_t* ptr = (uint32_t*) 0x08008004;
-	void (*pf)() = (void (*)()) *ptr;
-
-	pf();
-}
-
 void myDelay()
 {
 	volatile int x = 0;
@@ -84,21 +111,4 @@ void myDelay()
 	}
 
 	return;
-}
-
-int main()
-{
-	HAL_Init();
-	LED_Init();
-	EXTI_Init();
-
-	while (1)
-	{
-		LED_ctrl(ON);
-		myDelay();
-		LED_ctrl(OFF);
-		myDelay();
-	}
-
-	return 0;
 }
