395222079f698d08acff0ba1254e266e02fdd652
anhkh04 <phamanhkhoa9803@gmail.com>
2025-06-04 16:31:38 +0700

    Turn ON Blue LED, 'jump' to App1 FW when pressing button

--- project2/BootLoader/Core/Src/main.c
+++ project2/BootLoader/Core/Src/main.c
@@ -1,114 +1,226 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2025 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
 #include "main.h"
-#include <strings.h>
-#define GPIOD_BASE_ADDR 0x40020C00
-#define GPIOA_BASE_ADDR 0x40020000
-#define EXTI_BASE_ADDR 0x40013C00
 
-typedef enum{
-	OFF, ON
-} LED_state;
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
 
-void LED_Init();
-void LED_ctrl(LED_state state);
-void myDelay();
-void custom_Handler();
-void EXTI_Init();
+/* USER CODE END Includes */
 
-int main()
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+
+/* USER CODE BEGIN PV */
+
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+/* USER CODE BEGIN PFP */
+
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
 {
-	HAL_Init();
-	LED_Init();
-	EXTI_Init();
 
-	while (1)
-	{
-		LED_ctrl(ON);
-		myDelay();
-		LED_ctrl(OFF);
-		myDelay();
-	}
+  /* USER CODE BEGIN 1 */
 
-	return 0;
+  /* USER CODE END 1 */
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  /* USER CODE BEGIN 2 */
+  HAL_GPIO_WritePin(GPIOD, GPIO_PIN_15, GPIO_PIN_SET);
+  uint32_t* ptr;
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  while (1)
+  {
+    /* USER CODE END WHILE */
+	  if (HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_0) == 1)
+	  {
+		  while (HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_0) == 1);
+		  HAL_Delay(100);
+		  HAL_GPIO_WritePin(GPIOD, GPIO_PIN_15, GPIO_PIN_RESET);
+		  ptr = (uint32_t*) 0x08004004; // Run App1
+		  break;
+	  }
+    /* USER CODE BEGIN 3 */
+  }
+  void (*fptr)() = (void (*)()) *ptr;
+  fptr();
+  /* USER CODE END 3 */
 }
 
-void custom_Handler()
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
 {
-	// Reset the PR Register to turn off the interrupt flag
-	uint32_t* EXTI_PR = (uint32_t*) (EXTI_BASE_ADDR + 0x14);
-	*EXTI_PR |= (0b1 << 0);
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
 
-	// Turn off Blue LED before jump
-	LED_ctrl(OFF);
+  /** Configure the main internal regulator output voltage
+  */
+  __HAL_RCC_PWR_CLK_ENABLE();
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
 
-	// Jump to Reset Handler function of App1
-	uint32_t* ptr = (uint32_t*) 0x08004004;
-	void (*pf)() = (void (*)()) *ptr;
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
 
-	pf();
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
+  {
+    Error_Handler();
+  }
 }
 
-void EXTI_Init()
+/**
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_GPIO_Init(void)
 {
-	__HAL_RCC_GPIOA_CLK_ENABLE();
-	// Declare GPIOA's MODER Register
-	uint32_t* GPIOA_MODER = (uint32_t*) (GPIOA_BASE_ADDR + 0x00);
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+  /* USER CODE BEGIN MX_GPIO_Init_1 */
 
-	// Set pin PA0 as INPUT
-	*GPIOA_MODER &= ~(0b11 << 0);
+  /* USER CODE END MX_GPIO_Init_1 */
 
-	// Declare EXTI line 0's FTSR Register and set to HIGH
-	uint32_t* EXTI_FTSR = (uint32_t*) (EXTI_BASE_ADDR + 0x0C);
-	*EXTI_FTSR |= (0b1 << 0);
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOD_CLK_ENABLE();
 
-	// Declare EXTI line 0's IMR Register and set to HIGH
-	uint32_t* EXTI_IMR = (uint32_t*) (EXTI_BASE_ADDR + 0x00);
-	*EXTI_IMR |= (0b1 << 0);
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOD, GPIO_PIN_15, GPIO_PIN_RESET);
 
-	// Set the NVIC ISER0 at position 6 to HIGH
-	uint32_t* NVIC_ISER0 = (uint32_t*) 0xE000E100;
-	*NVIC_ISER0 |= (0b1 << 6);
+  /*Configure GPIO pin : PA0 */
+  GPIO_InitStruct.Pin = GPIO_PIN_0;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
-	memcpy(0x20000000, 0x08000000, 0x198);
+  /*Configure GPIO pin : PD15 */
+  GPIO_InitStruct.Pin = GPIO_PIN_15;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOD, &GPIO_InitStruct);
 
-	uint32_t* VTOR = (uint32_t*) 0xE000ED08;
-	*VTOR = 0x20000000;
+  /* USER CODE BEGIN MX_GPIO_Init_2 */
 
-	uint32_t* fp = (uint32_t*) 0x20000058;
-	*fp = custom_Handler;
+  /* USER CODE END MX_GPIO_Init_2 */
 }
 
-void myDelay()
-{
-	volatile int x = 0;
-	for (int i = 0; i < 1e6; i++) {
-		x++;
-	}
+/* USER CODE BEGIN 4 */
 
-	return;
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
 }
 
-void LED_ctrl(LED_state state)
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
 {
-	// Declare GPIOD's ODR Register
-	uint32_t* GPIOD_ODR = (uint32_t*) (GPIOD_BASE_ADDR + 0x14);
-	// Set output state for pin PD15
-	if (state == ON)
-	{
-		*GPIOD_ODR |= (0b1 << 15);
-	}
-	else if (state == OFF)
-	{
-		*GPIOD_ODR &= ~(0b1 << 15);
-	}
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
 }
-
-void LED_Init()
-{
-	__HAL_RCC_GPIOD_CLK_ENABLE();
-	// Declare GPIOD's MODER Register
-	uint32_t* GPIOD_MODER = (uint32_t*) (GPIOD_BASE_ADDR + 0x00);
-	// Clear pin PD15
-	*GPIOD_MODER &= ~(0b11 << 30);
-	// Set pin PD15 as OUTPUT
-	*GPIOD_MODER |= (0b01 << 30);
-}
+#endif /* USE_FULL_ASSERT */
