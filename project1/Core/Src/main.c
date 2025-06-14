6b78b3ae48d9f23d8455788882878f27dfa79561
anhkh04 <phamanhkhoa9803@gmail.com>
2025-06-14 17:06:38 +0700

    upd erase and load firmware function

--- project1/Core/Src/main.c
+++ project1/Core/Src/main.c
@@ -1,35 +1,55 @@
 #include "main.h"
-#define FLASH_BASE_ADDR 0x40023C00
+#define FLASH_R_BASE_ADDR 0x40023C00
 void flash_erase_sector(int sec_num);
 void flash_program(uint8_t* addr, uint8_t val);
+void unlock_flash_CR();
 
 int main()
 {
 
 
+	while (1)
+	{
+
+	}
 	return 0;
+}
+
+void unlock_flash_CR()
+{
+	uint32_t* FLASH_CR = (uint32_t*) (FLASH_R_BASE_ADDR + 0x10);
+	uint32_t* FLASH_KEYR = (uint32_t*) (FLASH_R_BASE_ADDR + 0x04);
+
+	if (((*FLASH_CR >> 31) & 1) == 1)
+	{
+		*FLASH_KEYR = 0x45670123;	// unlock the FLASH CR
+		*FLASH_KEYR = 0xCDEF89AB;
+	}
 }
 
 void flash_erase_sector(int sec_num)
 {
-	uint32_t* FLASH_SR = (uint32_t*) (FLASH_BASE_ADDR + 0x0C);
+	if (sec_num > 7) { return; }
+	unlock_flash_CR();
+
+	uint32_t* FLASH_SR = (uint32_t*) (FLASH_R_BASE_ADDR + 0x0C);
 	while (((*FLASH_SR >> 16) & 1) == 1);	// check the BSY bit
 
-	uint32_t* FLASH_CR = (uint32_t*) (FLASH_BASE_ADDR + 0x10);
+	uint32_t* FLASH_CR = (uint32_t*) (FLASH_R_BASE_ADDR + 0x10);
 	*FLASH_CR |= (1 << 1);	// set the SER bit
 	*FLASH_CR |= (sec_num << 3);	// select the section as sec_num
-	*FLASH_CR |= (1 << 16)	// set the STRT bit
+	*FLASH_CR |= (1 << 16);	// set the STRT bit
 	while (((*FLASH_SR >> 16) & 1) == 1);	// wait for BSY bit to be cleared
-
-	uint32_t* FLASH_KEYR = (uint32_t*) (FLASH_BASE + 0x04);
 }
 
 void flash_program(uint8_t* addr, uint8_t val)
 {
-	uint32_t* FLASH_SR = (uint32_t*) (FLASH_BASE_ADDR + 0x0C);
+	unlock_flash_CR();
+
+	uint32_t* FLASH_SR = (uint32_t*) (FLASH_R_BASE_ADDR + 0x0C);
 	while (((*FLASH_SR >> 16) & 1) == 1);	// check the BSY bit
 
-	uint32_t* FLASH_CR = (uint32_t*) (FLASH_BASE_ADDR + 0x10);
+	uint32_t* FLASH_CR = (uint32_t*) (FLASH_R_BASE_ADDR + 0x10);
 	*FLASH_CR |= (1 << 0);	// set the PG bit
 	*addr = val;
 	while (((*FLASH_SR >> 16) & 1) == 1);	// wait for BSY bit to be cleared
