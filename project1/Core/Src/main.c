7122150763a93cce3e45155618dee01d25f48459
anhkh04 <phamanhkhoa9803@gmail.com>
2025-06-15 11:43:32 +0700

    Use HC-SR04 sensor to calculate the distance in centimeters successfully

--- project1/Core/Src/main.c
+++ project1/Core/Src/main.c
@@ -8,51 +8,41 @@ void TIM2_Init();
 void delay_us(uint32_t us);
 void delay_ms(uint32_t ms);
 void trig_HCSR04();
-uint32_t cal_time();
-uint32_t cal_distance();
+void cal_distance();
 
 float distance = 0;
-uint32_t time = 0;
 
 int main()
 {
 	HAL_Init();
 	HCSR04_Init();
 	TIM2_Init();
-
 	while (1)
 	{
 		trig_HCSR04();
-		time = cal_time();
-		distance = cal_distance();
-		delay_ms(2000);
+		cal_distance();
+		delay_ms(1000);
 	}
 	return 0;
 }
 
-uint32_t cal_distance()
+void cal_distance()
 {
-	float v = (343 * 100) / (float) 10e6;	// v = 0.0343 cm/us
-	float d = time * v;
-	return d/2;
-}
-
-uint32_t cal_time()
-{
-	uint32_t* GPIOC_IDR = (uint32_t*) (GPIOC_BASE_ADDR + 0x10);
+	volatile uint32_t* GPIOC_IDR = (uint32_t*) (GPIOC_BASE_ADDR + 0x10);
 	while (((*GPIOC_IDR >> 6) & 1) == 0);	// wait until ECHO is HIGH
 
-	uint32_t* TIM2_CNT = (uint32_t*) (TIM2_BASE_ADDR + 0x24);
-	*TIM2_CNT = 0;	// set counting back from 0
+	volatile uint32_t* TIM2_CNT = (uint32_t*) (TIM2_BASE_ADDR + 0x24);
+	*TIM2_CNT = 0;	// set counter counting back from 0
 	while (((*GPIOC_IDR >> 6) & 1) == 1);	// Keep counting until ECHO is LOW
-	return *TIM2_CNT;
+	uint32_t time = *TIM2_CNT;	// save the time
+	distance = time / 58;	// use equation 3 in datasheet to calculate distance
 }
 
 void trig_HCSR04()
 {
 	uint32_t* GPIOC_ODR = (uint32_t*) (GPIOC_BASE_ADDR + 0x14);
 	*GPIOC_ODR |= (1 << 7);		// set TRIG to HIGH
-	delay_us(9);
+	delay_us(11);
 	*GPIOC_ODR &= ~(1 << 7);	// clear TRIG to LOW
 }
 
@@ -67,8 +57,7 @@ void delay_ms(uint32_t ms)
 {
 	uint32_t* TIM2_CNT = (uint32_t*) (TIM2_BASE_ADDR + 0x24);
 	*TIM2_CNT = 0;
-	uint32_t time = ms * 1000;
-	while (*TIM2_CNT < time);
+	while (*TIM2_CNT < (ms * 1000));
 }
 
 void TIM2_Init()
@@ -76,6 +65,7 @@ void TIM2_Init()
 	__HAL_RCC_TIM2_CLK_ENABLE();
 	uint32_t* TIM2_CR1 = (uint32_t*) (TIM2_BASE_ADDR + 0x00);
 	*TIM2_CR1 &= ~(1 << 4);	// up-counter
+	*TIM2_CR1 &= ~(1 << 0); // make sure CEN = 0 (stop timer)
 
 	uint32_t* TIM2_PSC = (uint32_t*) (TIM2_BASE_ADDR + 0x28);
 	*TIM2_PSC = 16 - 1;		// PSC = N - 1, set CLK_TIM2 = 1 MHz
@@ -93,3 +83,4 @@ void HCSR04_Init()
 	*GPIOC_MODER &= ~(0b1111 << 12);	// set PC6 as INPUT ~ ECHO
 	*GPIOC_MODER |= (0b01 << 14);	// set PC7 as OUTPUT ~ TRIG
 }
+
