e3bf7f24383458a001a4c7a6a5638660e0ee99ce
anhkh04 <phamanhkhoa9803@gmail.com>
2025-07-16 17:04:38 +0700

    implement transceive func to LSM303AGR

--- project1/Core/Src/main.c
+++ project1/Core/Src/main.c
@@ -1,167 +1,177 @@
 #include "main.h"
-#define TIM1_BASE_ADDR 0x40010000
-void LED_Ctrl(char on);
-void LED_Init();
-void TIM1_Init();
-void delay_second(uint32_t time);
-void delay_microsecond(uint32_t time);
-void delay_milisecond(uint32_t time);
-void TIM1_UP_TIM10_IRQHandler();
-void INT_delay_second(uint32_t time);
-void systick_init();
-void systick_delay_1s();
-uint32_t cnt = 0;
 
+#define I2C1_BASE_ADDR 0x40005400
+#define GPIOB_BASE_ADDR 0x40020400
+#define ACCEL_ADDR 0x19
+#define MAGNE_ADDR 0x1E
+typedef enum
+{
+	ACK, NACK
+} check_t;
+uint8_t master_read(uint8_t slave_addr, uint8_t reg_addr);
+uint8_t master_receive(uint8_t slave_addr, uint8_t reg_addr);
+uint8_t master_transfer(uint8_t slave_addr, uint8_t reg_addr, uint8_t data);
+void I2C_Init();
+
+uint8_t data = 0;
 int main()
 {
-	LED_Init();
-	TIM1_Init();
-	systick_init();
+	HAL_Init();
+	I2C_Init();
+
 	while (1)
 	{
-		LED_Ctrl(1);
-		systick_delay_1s();
-//		INT_delay_second(2);
-		LED_Ctrl(0);
-		systick_delay_1s();
-//		INT_delay_second(4);
+		data = master_read(ACCEL_ADDR, 0x20);
+		data = master_read(MAGNE_ADDR, 0x60);
 	}
 	return 0;
 }
 
-void systick_delay_1s()
+uint8_t master_read(uint8_t slave_addr, uint8_t reg_addr)
 {
-	uint32_t* STK_CTRL  = (uint32_t*) (0xE000E010);
-//	*STK_CTRL &= ~(1 << 16);
-	while (((*STK_CTRL >> 16) & 1) == 0);
-	*STK_CTRL &= ~(1 << 16);
+	uint16_t* I2C_CR1 = (uint16_t*) (I2C1_BASE_ADDR + 0x00);
+	uint16_t* I2C_DR  = (uint16_t*) (I2C1_BASE_ADDR + 0x10);
+	uint16_t* I2C_SR1 = (uint16_t*) (I2C1_BASE_ADDR + 0x14);
+	uint16_t* I2C_SR2 = (uint16_t*) (I2C1_BASE_ADDR + 0x18);
+
+	/* wait until bus is free */
+//	while (((*I2C_SR2 >> 1) & 1) == 1);
+
+	/* send start bit */
+	*I2C_CR1 |= 1 << 8;
+
+	/* wait for start is generated */
+	while ((*I2C_SR1 & 1) == 0);
+
+	/* send slave address + write mode */
+	*I2C_DR  = (slave_addr << 1);
+
+	/* wait for slave address is transmitted */
+	while (((*I2C_SR1 >> 1) & 1) == 0);
+	volatile uint16_t tmp = *I2C_SR2;
+
+	/* check ACK */
+	if (((*I2C_SR1 >> 10) & 1) == 1) { return 1; }
+
+	/* send register address */
+	*I2C_DR = reg_addr;
+
+	/* wait for register address is transmitted */
+	while (((*I2C_SR1 >> 7) & 1) == 0);
+
+	/* check ACK */
+	if (((*I2C_SR1 >> 10) & 1) == 1) { return 1; }
+
+	/* send start bit */
+	*I2C_CR1 |= 1 << 8;
+
+	/* wait for start is generated */
+	while ((*I2C_SR1 & 1) == 0);
+
+	/* send slave address + read mode */
+	*I2C_DR = (slave_addr << 1) | 0x01;
+
+	/* wait for slave address is transmitted */
+	while (((*I2C_SR1 >> 1) & 1) == 0);
+	tmp = *I2C_SR2;
+
+	/* check ACK */
+	if (((*I2C_SR1 >> 10) & 1) == 1) { return 1; }
+
+	/* read data */
+	volatile uint8_t data = *I2C_DR;	// Breakpoint here
+
+	/* send stop bit */
+	*I2C_CR1 |= 1 << 9;
+
+	return data;
 }
 
-void systick_init()
+uint8_t master_receive(uint8_t slave_addr, uint8_t reg_addr)
 {
-	uint32_t* STK_CTRL  = (uint32_t*) (0xE000E010);
-	uint32_t* STK_LOAD  = (uint32_t*) (0xE000E014);
-	/* select clock = 16MHz & enable interrupt */
-	*STK_CTRL |= 1 << 1;
-	/* reload every 1 second */
-	*STK_LOAD = 2000 * 1000 - 1;
-	/* enable counter */
-	*STK_CTRL |= 1 << 0;
+	uint16_t* I2C_CR1 = (uint16_t*) (I2C1_BASE_ADDR + 0x00);
+	uint16_t* I2C_DR  = (uint16_t*) (I2C1_BASE_ADDR + 0x10);
+	uint16_t* I2C_SR1 = (uint16_t*) (I2C1_BASE_ADDR + 0x14);
+	uint16_t* I2C_SR2 = (uint16_t*) (I2C1_BASE_ADDR + 0x18);
+	/* send start */
+	*I2C_CR1 |= 1 << 8;
+	while ((*I2C_SR1 & 1) == 0);
+	/* send slave address + write bit */
+	*I2C_DR = slave_addr << 1;
+	while (((*I2C_SR1 >> 1) & 1) == 0);
+	uint16_t tmp = *I2C_SR2;
+	if (((*I2C_SR1 >> 10) & 1) == 1) return NACK;
+	/* send register address */
+	*I2C_DR = reg_addr;
+	while (((*I2C_SR1 >> 7) & 1) == 0);
+	if (((*I2C_SR1 >> 10) & 1) == 1) return NACK;
+	/* send start */
+	*I2C_CR1 |= 1 << 8;
+	while ((*I2C_SR1 & 1) == 0);
+	/* send slave address + read bit */
+	*I2C_DR = (slave_addr << 1) | 0x01;
+	while (((*I2C_SR1 >> 1) & 1) == 0);
+	tmp = *I2C_SR2;
+	if (((*I2C_SR1 >> 10) & 1) == 1) return NACK;
+	/* read data */
+	uint8_t data = *I2C_DR;
+	/* send stop */
+	*I2C_CR1 |= 1 << 9;
+
+	return data;
 }
 
-void INT_delay_second(uint32_t time)
+uint8_t master_transfer(uint8_t slave_addr, uint8_t reg_addr, uint8_t data)
 {
-	cnt = 0;
-	while (cnt < time);
+	uint16_t* I2C_CR1 = (uint16_t*) (I2C1_BASE_ADDR + 0x00);
+	uint16_t* I2C_DR  = (uint16_t*) (I2C1_BASE_ADDR + 0x10);
+	uint16_t* I2C_SR1 = (uint16_t*) (I2C1_BASE_ADDR + 0x14);
+	uint16_t* I2C_SR2 = (uint16_t*) (I2C1_BASE_ADDR + 0x18);
+	/* send start */
+	*I2C_CR1 |= 1 << 8;
+	while ((*I2C_SR1 & 1) == 0);
+	/* send slave address + write bit */
+	*I2C_DR = slave_addr << 1;
+	while (((*I2C_SR1 >> 1) & 1) == 0);
+	uint8_t tmp = *I2C_SR2;
+	if (((*I2C_SR1 >> 10) & 1) == 1) return NACK;
+	/* send register address */
+	*I2C_DR = reg_addr;
+	while (((*I2C_SR1 >> 7) & 1) == 0);
+	if (((*I2C_SR1 >> 10) & 1) == 1) return NACK;
+	/* send data */
+	*I2C_DR = data;
+	while (((*I2C_SR1 >> 7) & 1) == 0);
+	if (((*I2C_SR1 >> 10) & 1) == 1) return NACK;
+	/* send stop */
+	*I2C_CR1 |= 1 << 9;
+	return ACK;
 }
 
-void TIM1_UP_TIM10_IRQHandler()
+/* I2C1
+ * PB6: SCL
+ * PB9: SDA
+ */
+void I2C_Init()
 {
-	uint16_t* TIM1_SR = (uint16_t*) (TIM1_BASE_ADDR + 0x10);
-	*TIM1_SR &= ~(1 << 0);
-	cnt++;
-}
+	__HAL_RCC_GPIOB_CLK_ENABLE();
+	uint32_t* GPIOB_MODER = (uint32_t*) (GPIOB_BASE_ADDR + 0x00);
+	uint32_t* GPIOB_AFRL  = (uint32_t*) (GPIOB_BASE_ADDR + 0x20);
+	uint32_t* GPIOB_AFRH  = (uint32_t*) (GPIOB_BASE_ADDR + 0x24);
 
-/* min: 1us -> max: 4095us */
-void delay_microsecond(uint32_t time)
-{
-	if (time == 0) return;
-	uint16_t* TIM1_SR = (uint16_t*) (TIM1_BASE_ADDR + 0x10);
-	uint16_t* TIM1_EGR = (uint16_t*) (TIM1_BASE_ADDR + 0x14);
-	uint16_t* TIM1_ARR = (uint16_t*) (TIM1_BASE_ADDR + 0x2C);
-	uint16_t* TIM1_PSC = (uint16_t*) (TIM1_BASE_ADDR + 0x28);
-	uint16_t* TIM1_CNT = (uint16_t*) (TIM1_BASE_ADDR + 0x24);
-	*TIM1_ARR = time * 16;
-	*TIM1_PSC = 1 - 1;
-	*TIM1_CNT = 0;
-	*TIM1_EGR |= 1 << 0;
-	*TIM1_SR &= ~(1 << 0);
-	while ((*TIM1_SR & 1) == 0);
-	*TIM1_SR &= ~(1 << 0);
-}
+	*GPIOB_MODER &= ~((0b11 << (6 * 2)) | (0b11 << (9 * 2)));
+	*GPIOB_MODER |= (0b10 << (6 * 2)) | (0b10 << (9 * 2));
 
-/* min: 1ms -> max: 131ms */
-void delay_milisecond(uint32_t time)
-{
-	if (time == 0) return;
-	uint16_t* TIM1_SR = (uint16_t*) (TIM1_BASE_ADDR + 0x10);
-	uint16_t* TIM1_EGR = (uint16_t*) (TIM1_BASE_ADDR + 0x14);
-	uint16_t* TIM1_ARR = (uint16_t*) (TIM1_BASE_ADDR + 0x2C);
-	uint16_t* TIM1_PSC = (uint16_t*) (TIM1_BASE_ADDR + 0x28);
-	uint16_t* TIM1_CNT = (uint16_t*) (TIM1_BASE_ADDR + 0x24);
-	if (time <= 65)
-	{
-		*TIM1_ARR = time * 1000;
-		*TIM1_PSC = 16 - 1;
-	}
-	else if (time > 65)
-	{
-		*TIM1_ARR = (time / 2) * 1000;
-		*TIM1_PSC = 32 - 1;
-	}
-	*TIM1_CNT = 0;
-	*TIM1_EGR |= 1 << 0;
-	*TIM1_SR &= ~(1 << 0);
-	while ((*TIM1_SR & 1) == 0);
-	*TIM1_SR &= ~(1 << 0);
-}
+	*GPIOB_AFRL &= ~(0xf << (6 * 4));
+	*GPIOB_AFRL |= 4 << (6 * 4);
+	*GPIOB_AFRH &= ~(0xf << 4);
+	*GPIOB_AFRH |= 4 << 4;
 
-/* min: 1s -> max: 131s */
-void delay_second(uint32_t time)
-{
-	if (time == 0) return;
-	uint16_t* TIM1_SR = (uint16_t*) (TIM1_BASE_ADDR + 0x10);
-	uint16_t* TIM1_EGR = (uint16_t*) (TIM1_BASE_ADDR + 0x14);
-	uint16_t* TIM1_ARR = (uint16_t*) (TIM1_BASE_ADDR + 0x2C);
-	uint16_t* TIM1_PSC = (uint16_t*) (TIM1_BASE_ADDR + 0x28);
-	if (time <= 65)
-	{
-		*TIM1_ARR = time * 1000;
-		*TIM1_PSC = 16000 - 1;
-	}
-	else if (time > 65)
-	{
-		*TIM1_ARR = (time / 2) * 1000;
-		*TIM1_PSC = 32000 - 1;
-	}
-	*TIM1_EGR |= 1 << 0;
-	*TIM1_SR &= ~(1 << 0);
-	while ((*TIM1_SR & 1) == 0);
-	*TIM1_SR &= ~(1 << 0);
-}
-
-void TIM1_Init()
-{
-	__HAL_RCC_TIM1_CLK_ENABLE();
-	uint16_t* TIM1_CR1  = (uint16_t*) (TIM1_BASE_ADDR + 0x00);
-	uint16_t* TIM1_DIER = (uint16_t*) (TIM1_BASE_ADDR + 0x0C);
-	uint16_t* TIM1_ARR = (uint16_t*) (TIM1_BASE_ADDR + 0x2C);
-	uint16_t* TIM1_PSC = (uint16_t*) (TIM1_BASE_ADDR + 0x28);
-	uint32_t* NVIC_ISER0 = (uint32_t*) (0xE000E100);
-	*TIM1_ARR = 1000;
-	*TIM1_PSC = 16000 - 1;
-	/* enable interrupt */
-	*TIM1_DIER |= 1 << 0;
-	*NVIC_ISER0 |= 1 << 25;
-	/* counter enable */
-	*TIM1_CR1 |= 1 << 0;
-}
-void LED_Init()
-{
-	__HAL_RCC_GPIOD_CLK_ENABLE();
-	uint32_t* GPIOD_MODER = (uint32_t*) (GPIOD_BASE + 0x00);
-	*GPIOD_MODER &= ~(0b11 << (15 * 2));
-	*GPIOD_MODER |= (0b01 << (15 * 2));
-}
-void LED_Ctrl(char on)
-{
-	uint32_t* GPIOD_ODR = (uint32_t*) (GPIOD_BASE + 0x14);
-	if (on)
-	{
-		*GPIOD_ODR |= 1 << 15;
-	}
-	else
-	{
-		*GPIOD_ODR &= ~(1 << 15);
-	}
+	__HAL_RCC_I2C1_CLK_ENABLE();
+	uint16_t* I2C_CR1 = (uint16_t*) (I2C1_BASE_ADDR + 0x00);
+	uint16_t* I2C_CR2 = (uint16_t*) (I2C1_BASE_ADDR + 0x04);
+	uint16_t* I2C_CCR = (uint16_t*) (I2C1_BASE_ADDR + 0x1C);
+	*I2C_CR2 |= 16 << 0;
+	*I2C_CCR = 80;
+	*I2C_CR1 |= 1 << 0;
 }
