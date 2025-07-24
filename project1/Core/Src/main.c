3380c0cf696f678b0930a1b4218d89699d830397
anhkh04 <phamanhkhoa9803@gmail.com>
2025-07-24 21:39:24 +0700

    using FreeRTOS on STM32

--- project1/Core/Src/main.c
+++ project1/Core/Src/main.c
@@ -1,59 +1,463 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2025 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
 #include "main.h"
+#include "cmsis_os.h"
+#include <string.h>
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
 
-#define ADC1_BASE_ADDR 0x40012000
-void ADC_Init();
-float readVin();
-float convert2Temp(float vin);
-float temp;
-int main()
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+UART_HandleTypeDef huart1;
+
+/* Definitions for task1 */
+osThreadId_t task1Handle;
+const osThreadAttr_t task1_attributes = {
+  .name = "task1",
+  .stack_size = 128 * 4,
+  .priority = (osPriority_t) osPriorityNormal,
+};
+/* Definitions for task2 */
+osThreadId_t task2Handle;
+const osThreadAttr_t task2_attributes = {
+  .name = "task2",
+  .stack_size = 128 * 4,
+  .priority = (osPriority_t) osPriorityLow,
+};
+/* Definitions for task3 */
+osThreadId_t task3Handle;
+const osThreadAttr_t task3_attributes = {
+  .name = "task3",
+  .stack_size = 128 * 4,
+  .priority = (osPriority_t) osPriorityLow,
+};
+/* Definitions for task4 */
+osThreadId_t task4Handle;
+const osThreadAttr_t task4_attributes = {
+  .name = "task4",
+  .stack_size = 128 * 4,
+  .priority = (osPriority_t) osPriorityLow,
+};
+/* Definitions for task5 */
+osThreadId_t task5Handle;
+const osThreadAttr_t task5_attributes = {
+  .name = "task5",
+  .stack_size = 128 * 4,
+  .priority = (osPriority_t) osPriorityLow,
+};
+/* Definitions for data_queue_ */
+osMessageQueueId_t data_queue_Handle;
+const osMessageQueueAttr_t data_queue__attributes = {
+  .name = "data_queue_"
+};
+/* Definitions for uart_lock_ */
+osMutexId_t uart_lock_Handle;
+const osMutexAttr_t uart_lock__attributes = {
+  .name = "uart_lock_"
+};
+/* USER CODE BEGIN PV */
+
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+static void MX_USART1_UART_Init(void);
+void task1_blink_red_led(void *argument);
+void task2_blink_blue_led(void *argument);
+void task3_send_hello(void *argument);
+void task4_send_xinchao(void *argument);
+void task5_read_data(void *argument);
+
+/* USER CODE BEGIN PFP */
+
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+void my_printf(char* str)
 {
-	HAL_Init();
-	ADC_Init();
-	while (1)
+	int str_len = strlen(str);
+	for (int i = 0; i < str_len; i++)
 	{
-		float tmp = readVin();
-		temp = convert2Temp(tmp);
-		HAL_Delay(1000);
+		HAL_UART_Transmit(&huart1, (uint8_t*) &str[i], 1, HAL_MAX_DELAY);
+		osDelay(100);
 	}
-	return 0;
+}
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+
+  /* USER CODE BEGIN 1 */
+
+  /* USER CODE END 1 */
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_USART1_UART_Init();
+  /* USER CODE BEGIN 2 */
+
+  /* USER CODE END 2 */
+
+  /* Init scheduler */
+  osKernelInitialize();
+  /* Create the mutex(es) */
+  /* creation of uart_lock_ */
+  uart_lock_Handle = osMutexNew(&uart_lock__attributes);
+
+  /* USER CODE BEGIN RTOS_MUTEX */
+  /* add mutexes, ... */
+  /* USER CODE END RTOS_MUTEX */
+
+  /* USER CODE BEGIN RTOS_SEMAPHORES */
+  /* add semaphores, ... */
+  /* USER CODE END RTOS_SEMAPHORES */
+
+  /* USER CODE BEGIN RTOS_TIMERS */
+  /* start timers, add new ones, ... */
+  /* USER CODE END RTOS_TIMERS */
+
+  /* Create the queue(s) */
+  /* creation of data_queue_ */
+  data_queue_Handle = osMessageQueueNew (16, sizeof(int), &data_queue__attributes);
+
+  /* USER CODE BEGIN RTOS_QUEUES */
+  /* add queues, ... */
+  /* USER CODE END RTOS_QUEUES */
+
+  /* Create the thread(s) */
+  /* creation of task1 */
+  task1Handle = osThreadNew(task1_blink_red_led, NULL, &task1_attributes);
+
+  /* creation of task2 */
+  task2Handle = osThreadNew(task2_blink_blue_led, NULL, &task2_attributes);
+
+  /* creation of task3 */
+  task3Handle = osThreadNew(task3_send_hello, NULL, &task3_attributes);
+
+  /* creation of task4 */
+  task4Handle = osThreadNew(task4_send_xinchao, NULL, &task4_attributes);
+
+  /* creation of task5 */
+  task5Handle = osThreadNew(task5_read_data, NULL, &task5_attributes);
+
+  /* USER CODE BEGIN RTOS_THREADS */
+  /* add threads, ... */
+  /* USER CODE END RTOS_THREADS */
+
+  /* USER CODE BEGIN RTOS_EVENTS */
+  /* add events, ... */
+  /* USER CODE END RTOS_EVENTS */
+
+  /* Start scheduler */
+  osKernelStart();
+
+  /* We should never get here as control is now taken by the scheduler */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  while (1)
+  {
+    /* USER CODE END WHILE */
+
+    /* USER CODE BEGIN 3 */
+  }
+  /* USER CODE END 3 */
 }
 
-float convert2Temp(float vin)
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
 {
-	float temp = ((vin - 0.76) / 2.5e-3) + 25;
-	return temp;
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Configure the main internal regulator output voltage
+  */
+  __HAL_RCC_PWR_CLK_ENABLE();
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
+  {
+    Error_Handler();
+  }
 }
 
-float readVin()
+/**
+  * @brief USART1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_USART1_UART_Init(void)
 {
-	uint32_t* ADC_SR   = (uint32_t*) (ADC1_BASE_ADDR + 0x00);
-	uint32_t* ADC_CR2  = (uint32_t*) (ADC1_BASE_ADDR + 0x08);
-	uint32_t* ADC_JDR1 = (uint32_t*) (ADC1_BASE_ADDR + 0x3C);
-	/* start conversion of injected channel */
-	*ADC_CR2 |= 1 << 22;
-	/* wait until conversion complete */
-	while (((*ADC_SR >> 2) & 1) == 0);
-	/* return conversion value */
-	uint16_t val = *ADC_JDR1;
-	float vin = (val * 3.0f) / 4095;
-	return vin;
+
+  /* USER CODE BEGIN USART1_Init 0 */
+
+  /* USER CODE END USART1_Init 0 */
+
+  /* USER CODE BEGIN USART1_Init 1 */
+
+  /* USER CODE END USART1_Init 1 */
+  huart1.Instance = USART1;
+  huart1.Init.BaudRate = 115200;
+  huart1.Init.WordLength = UART_WORDLENGTH_8B;
+  huart1.Init.StopBits = UART_STOPBITS_1;
+  huart1.Init.Parity = UART_PARITY_NONE;
+  huart1.Init.Mode = UART_MODE_TX_RX;
+  huart1.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart1.Init.OverSampling = UART_OVERSAMPLING_16;
+  if (HAL_UART_Init(&huart1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN USART1_Init 2 */
+
+  /* USER CODE END USART1_Init 2 */
+
 }
 
-void ADC_Init()
+/**
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_GPIO_Init(void)
 {
-	__HAL_RCC_ADC1_CLK_ENABLE();
-	uint32_t* ADC_CR2   = (uint32_t*) (ADC1_BASE_ADDR + 0x08);
-	uint32_t* ADC_JSQR  = (uint32_t*) (ADC1_BASE_ADDR + 0x38);
-	uint32_t* ADC_CCR   = (uint32_t*) (ADC1_BASE_ADDR + 0x04 + 0x300);
-	uint32_t* ADC_SMPR1 = (uint32_t*) (ADC1_BASE_ADDR + 0x0C);
-	/* select 1 conversion */
-	*ADC_JSQR &= ~(0b11 << 20);
-	/* select src ADC_16 */
-	*ADC_JSQR |= 16 << 15;
-	/* enable temp. sensor */
-	*ADC_CCR |= 1 << 23;
-	/* set 15 ADC CLK cycles for 12 bit resolution */
-	*ADC_SMPR1 |= 0b001 << 18;
-	/* enable ADC */
-	*ADC_CR2 |= 1 << 0;
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+  /* USER CODE BEGIN MX_GPIO_Init_1 */
+
+  /* USER CODE END MX_GPIO_Init_1 */
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOD_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOD, GPIO_PIN_14|GPIO_PIN_15, GPIO_PIN_RESET);
+
+  /*Configure GPIO pins : PD14 PD15 */
+  GPIO_InitStruct.Pin = GPIO_PIN_14|GPIO_PIN_15;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOD, &GPIO_InitStruct);
+
+  /* USER CODE BEGIN MX_GPIO_Init_2 */
+
+  /* USER CODE END MX_GPIO_Init_2 */
 }
+
+/* USER CODE BEGIN 4 */
+
+/* USER CODE END 4 */
+
+/* USER CODE BEGIN Header_task1_blink_red_led */
+/**
+  * @brief  Function implementing the task1 thread.
+  * @param  argument: Not used
+  * @retval None
+  */
+/* USER CODE END Header_task1_blink_red_led */
+void task1_blink_red_led(void *argument)
+{
+  /* USER CODE BEGIN 5 */
+  /* Infinite loop */
+  for(;;)
+  {
+	  HAL_GPIO_TogglePin(GPIOD, GPIO_PIN_14);
+    osDelay(1000);
+  }
+  /* USER CODE END 5 */
+}
+
+/* USER CODE BEGIN Header_task2_blink_blue_led */
+/**
+* @brief Function implementing the task2 thread.
+* @param argument: Not used
+* @retval None
+*/
+/* USER CODE END Header_task2_blink_blue_led */
+void task2_blink_blue_led(void *argument)
+{
+  /* USER CODE BEGIN task2_blink_blue_led */
+  /* Infinite loop */
+  for(;;)
+  {
+	  HAL_GPIO_TogglePin(GPIOD, GPIO_PIN_15);
+    osDelay(3000);
+  }
+  /* USER CODE END task2_blink_blue_led */
+}
+
+/* USER CODE BEGIN Header_task3_send_hello */
+/**
+* @brief Function implementing the task3 thread.
+* @param argument: Not used
+* @retval None
+*/
+/* USER CODE END Header_task3_send_hello */
+void task3_send_hello(void *argument)
+{
+  /* USER CODE BEGIN task3_send_hello */
+  /* Infinite loop */
+  for(;;)
+  {
+	  osMutexAcquire(uart_lock_Handle, HAL_MAX_DELAY);
+	  my_printf("hello\n");
+	  osMutexRelease(uart_lock_Handle);
+    osDelay(1000);
+  }
+  /* USER CODE END task3_send_hello */
+}
+
+/* USER CODE BEGIN Header_task4_send_xinchao */
+/**
+* @brief Function implementing the task4 thread.
+* @param argument: Not used
+* @retval None
+*/
+/* USER CODE END Header_task4_send_xinchao */
+void task4_send_xinchao(void *argument)
+{
+  /* USER CODE BEGIN task4_send_xinchao */
+  /* Infinite loop */
+  int tmp = 0;
+	for(;;)
+  {
+	  osMessageQueueGet(data_queue_Handle, &tmp, 0, HAL_MAX_DELAY);
+	  osMutexAcquire(uart_lock_Handle, HAL_MAX_DELAY);
+	  my_printf("xin chao\n");
+	  osMutexRelease(uart_lock_Handle);
+    osDelay(1000);
+  }
+  /* USER CODE END task4_send_xinchao */
+}
+
+/* USER CODE BEGIN Header_task5_read_data */
+/**
+* @brief Function implementing the task5 thread.
+* @param argument: Not used
+* @retval None
+*/
+/* USER CODE END Header_task5_read_data */
+void task5_read_data(void *argument)
+{
+  /* USER CODE BEGIN task5_read_data */
+  /* Infinite loop */
+	int cnt = 0;
+  for(;;)
+  {
+	  cnt++;
+	  osMessageQueuePut(data_queue_Handle, &cnt, 0, HAL_MAX_DELAY);
+    osDelay(500);
+  }
+  /* USER CODE END task5_read_data */
+}
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
+#ifdef USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
